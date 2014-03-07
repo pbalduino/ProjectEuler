f86e26dd64b044f3f028a1c4df29dd3acf4d33c8
Plinio Balduino <pbalduino+github@gmail.com>
2014-03-07 18:06:39 -0300

    Source

--- 001/clojure/euler001.clj
+++ 001/clojure/euler001.clj
@@ -4,6 +4,8 @@
 ;
 ; If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 ; Find the sum of all the multiples of 3 or 5 below 1000.
+;
+; Source: https://projecteuler.net/problem=1
 
 (defn by-n [n]
   (fn [x]
