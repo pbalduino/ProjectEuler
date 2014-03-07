2b5b8756fa64efa68f92546cfaafa308290bcbd7
Plinio Balduino <pbalduino+github@gmail.com>
2014-03-07 18:06:48 -0300

    Solução 2

--- 001/ruby/euler001.rb
+++ 001/ruby/euler001.rb
@@ -1,5 +1,11 @@
-# http://projecteuler.net/problem=1
-  
+# Problem 1 - Multiples of 3 and 5
+
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+# Source: http://projecteuler.net/problem=1
+
+# Solução 1
 sum = 0
 
 (0..999).each do |n|
@@ -9,3 +15,6 @@ sum = 0
 end
 
 puts sum
+
+# Solução 2
+puts (1..999).to_a.keep_if{|n| n % 3 == 0 || n % 3 == 5}.reduce(:+)
